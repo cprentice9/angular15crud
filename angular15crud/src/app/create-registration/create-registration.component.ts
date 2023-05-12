cb19f16a1b701bd900f919da7ff9a16b252db0d8
Connor Prentice <connurp@gmail.com>
2023-05-12 00:26:22 -0500

    form reactive + added logic for bmi calculator

--- angular15crud/src/app/create-registration/create-registration.component.ts
+++ angular15crud/src/app/create-registration/create-registration.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-registration',
   templateUrl: './create-registration.component.html',
   styleUrls: ['./create-registration.component.scss']
 })
-export class CreateRegistrationComponent {
+export class CreateRegistrationComponent implements OnInit {
   public packages: string[] = ["Monthly", "Quarterly", "Yearly"];
   public genders: string[] = ["Male", "Female"];
   public importantList: string[] = [
@@ -15,5 +16,58 @@ export class CreateRegistrationComponent {
     "Better digestion",
     "Sugar reduction",
     "Fitness"
-  ]
+  ];
+
+  public registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+  }
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phoneNumber: [''],
+      weight: [''],
+      height: [''],
+      bmi: [''],
+      bmiResult: [''],
+      gender: [''],
+      requireTrainer: [''],
+      package: [''],
+      important: [''],
+      beenToGym: [''],
+      enquiryDate: [''],
+  });
+
+  this.registerForm.controls['height'].valueChanges.subscribe(res => {
+    this.calculateBmi(res);
+  })
+}
+submit(){
+  console.log(this.registerForm.value);
+}
+
+calculateBmi(heightValue: number){
+  const weight = this.registerForm.value.weight;
+  const height = heightValue;
+  const bmi = weight / (height * height);
+  this.registerForm.controls['bmi'].patchValue(bmi);
+  switch (true) {
+    case bmi < 18.5:
+      this.registerForm.controls['bmiResult'].patchValue("Underweight");
+      break;
+    case (bmi >= 18.5 && bmi < 25):
+      this.registerForm.controls['bmiResult'].patchValue("Normal Weight");
+      break;
+    case (bmi >= 25 && bmi < 30):
+      this.registerForm.controls['bmiResult'].patchValue("Overweight");
+      break;
+
+      default:
+        this.registerForm.controls['bmiResult'].patchValue("Obese");
+        break;
+}
+}
 }
