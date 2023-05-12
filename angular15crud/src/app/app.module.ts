4733acd5091a9e708a80919d977cbf0b33ae16ae
Connor Prentice <connurp@gmail.com>
2023-05-11 21:33:13 -0500

    added routing and material toolbar

--- angular15crud/src/app/app.module.ts
+++ angular15crud/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { NgConfirmModule } from 'ng-confirm-box';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +25,10 @@ import { NgConfirmModule } from 'ng-confirm-box';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
 
     NgToastModule,
     NgConfirmModule
